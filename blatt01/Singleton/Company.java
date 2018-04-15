1270cd6217335cabefcb46e5a5ac567674f9fa82
Alexander Tepe <alexander.tepe@hotmail.de>
2018-04-15 14:25:20 +0200

    added comments to Company class

--- blatt01/Singleton/Company.java
+++ blatt01/Singleton/Company.java
@@ -4,17 +4,26 @@ public class Company {
     private double stockprice;
     private Ticker ticker;
 
+    // Constructor
     public Company(String name, double stockprice) {
         this.name = name;
         this.stockprice = stockprice;
         this.ticker = Ticker.getInstance();
     }
 
+    /**
+     * @brief changes stockprice variable and calls ticker.print()
+     * @param d new stockprice
+     */
     public void changeStockPrice(double d) {
         this.stockprice = d;
         ticker.print(this.name + " " + this.stockprice);
     }
 
+    /**
+     * @brief Method to call when company is deleted
+     * @throws Throwable
+     */
     @Override
     protected void finalize() throws Throwable {
         super.finalize();
