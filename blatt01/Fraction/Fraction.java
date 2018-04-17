bd70898faded320f02d60484a7d2b5a3db20f93f
lalilu1410 <lisa.strzelecki@web.de>
2018-04-17 16:35:26 +0200

    Funktioniert jetzt richtig

--- blatt01/Fraction/Fraction.java
+++ blatt01/Fraction/Fraction.java
@@ -1,61 +1,94 @@
-
 public class Fraction {
+    private int numerator;  //Zaehler
+    private int denominator;  // Nenner
 
-	private int numerator;  //Zaehler
-	private int denominator;  // Nenner
-	
-	public Fraction(int num) {
-		this(num, 1);
-	}
-	
-	public Fraction(int numerator, int denominator) {
-		int ggt = euclid(numerator, denominator);
-		
-		this.numerator = numerator/ggt;
-		this.denominator = denominator/ggt;
-	}
-	
-	public Fraction multiply(int factor) {
-		int numeratorNew = this.numerator * factor;
-		Fraction result = new Fraction (numeratorNew, this.denominator);
-		return result;
-	}
-	
-	public Fraction multiply(Fraction factor) {
-		int numeratorNew = this.numerator * factor.numerator;
-		int denominatorNew = this.denominator * factor.denominator;
-		Fraction result = new Fraction(numeratorNew, denominatorNew);
-		return result;
-	}
-	
-	public Fraction divide(Fraction divisor) {
-		int numeratorNew = this.numerator * divisor.denominator;
-		int denominatorNew = this.denominator * divisor.numerator;
-		Fraction result = new Fraction(numeratorNew, denominatorNew);
-		return result;
-	}
-	
-	public Fraction multiply(Fraction... factors) {
-		Fraction result = new Fraction(this.numerator, this.denominator);
-		for(int i = 0; i < factors.length; i++) {
-			result.numerator *= factors[i].numerator;
-			result.denominator *= factors[i].denominator;
-		}
-		return result;
-	}
-	
-	public String toString() {
-		String result = "";
-		result = result + this.numerator + "/" + this.denominator;
-		return result;
-	}
-	
-	private int euclid(int a, int b) {
-		if (b == 0) {
-			return a;
-		} else {
-			return euclid(b, (a%b));
-		}
-	}
-	
+    /**
+     * @brief creates a new fraction from an int
+     * @param num
+     */
+    public Fraction(int num) {
+        this(num, 1);
+    }
+
+    /**
+     * @brief creates a new fraction
+     * @param numerator
+     * @param denominator
+     */
+    public Fraction(int numerator, int denominator) {
+        int ggt = euclid(numerator, denominator);
+
+        this.numerator = numerator/ggt;
+        this.denominator = denominator/ggt;
+    }
+
+    /**
+     * @brief multiplies the fraction with a given factor
+     * @param factor
+     * @return
+     */
+    public Fraction multiply(int factor) {
+        int numeratorNew = this.numerator * factor;
+        Fraction result = new Fraction (numeratorNew, this.denominator);
+        return result;
+    }
+
+    /**
+     * @brief multiplies the fraction with another given fraction
+     * @param factor
+     * @return
+     */
+    public Fraction multiply(Fraction factor) {
+        int numeratorNew = this.numerator * factor.numerator;
+        int denominatorNew = this.denominator * factor.denominator;
+        Fraction result = new Fraction(numeratorNew, denominatorNew);
+        return result;
+    }
+
+    /**
+     * @brief divides the fraction by another given fraction
+     * @param divisor
+     * @return
+     */
+    public Fraction divide(Fraction divisor) {
+        int numeratorNew = this.numerator * divisor.denominator;
+        int denominatorNew = this.denominator * divisor.numerator;
+        Fraction result = new Fraction(numeratorNew, denominatorNew);
+        return result;
+    }
+
+    /**
+     * @brief multiplies the fraction with multiple fractions from a given list of Fractions
+     * @param factors
+     * @return
+     */
+    public Fraction multiply(Fraction... factors) {
+        int numeratorNew = this.numerator;
+        int denominatorNew = this.denominator;
+        for(int i = 0; i < factors.length; i++) {
+            numeratorNew *= factors[i].numerator;
+            denominatorNew *= factors[i].denominator;
+        }
+        Fraction result = new Fraction(numeratorNew, denominatorNew);
+        return result;
+    }
+
+    /**
+     * @brief creates a String
+     * @return
+     */
+    public String toString() {
+        String result = "";
+        result = result + this.numerator + "/" + this.denominator;
+        return result;
+    }
+
+    private int euclid(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return euclid(b, (a%b));
+        }
+    }
+
 }
