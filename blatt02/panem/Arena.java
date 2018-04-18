58c8a50985d46df9e7028b45beaf83ba6fc5ee25
Alexander Tepe <alexander.tepe@hotmail.de>
2018-04-18 11:35:49 +0200

    WIP on Arena

--- blatt02/panem/Arena.java
+++ blatt02/panem/Arena.java
@@ -19,11 +19,47 @@ public class Arena {
      * @return a slice of the arena occupied with given tribute
      */
     public int getArea(Tribut tribute) {
+        // if tribute is inside arena:
         if(checkCoordinates(tribute)) {
-            System.out.println("doing stuff");
-            return 1;
+            double a = Math.abs(tribute.getX());
+            double b = Math.abs(tribute.getY());
+            int alpha = (int)Math.toDegrees(Math.sin(a/b));
+            if (alpha >= 0 && alpha < 30) {
+                return 1;
+            }
+            else if (alpha >= 30 && alpha < 60) {
+                return 2;
+            }
+            else if (alpha >= 60 && alpha < 90) {
+                return 3;
+            }
+            else if (alpha >= 90 && alpha < 120) {
+                return 4;
+            }
+            else if (alpha >= 120 && alpha < 150) {
+                return 5;
+            }
+            else if (alpha >= 180 && alpha < 210) {
+                return 6;
+            }
+            else if (alpha >= 210 && alpha < 240) {
+                return 7;
+            }
+            else if (alpha >= 240 && alpha < 270) {
+                return 8;
+            }
+            else if (alpha >= 270 && alpha < 300) {
+                return 9;
+            }
+            else if (alpha >= 300 && alpha < 330) {
+                return 10;
+            }
+            else {
+                return 11;
+            }
         }
         else {
+            // errorcode: outside arena
             return -1;
         }
     }
@@ -34,6 +70,16 @@ public class Arena {
      * @return true, if tribut is inside arena, else false
      */
     public boolean checkCoordinates(Tribut tribut) {
-        return Math.sqrt(Math.pow(tribut.getX(), 2.0) + Math.pow(tribut.getY(), 2.0)) > 1.5;
+        return getDistance(tribut.getX(), tribut.getY()) < 1.5;
+    }
+
+    /**
+     * @brief calculates the euclidian distance between x and y
+     * @param x
+     * @param y
+     * @return distance
+     */
+    public double getDistance(double x, double y) {
+        return Math.sqrt(Math.pow(x, 2.0) + Math.pow(y, 2.0));
     }
 }
