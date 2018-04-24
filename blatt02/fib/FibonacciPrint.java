1e4d6b1f4cfc7de6335f5576daa1b43d75ea2986
lalilu1410 <lisa.strzelecki@web.de>
2018-04-24 16:59:59 +0200

    fixed output

--- blatt02/fib/FibonacciPrint.java
+++ blatt02/fib/FibonacciPrint.java
@@ -5,14 +5,12 @@ public class FibonacciPrint {
         if (args[0] == null) {
             System.err.println("Usage: java FibonacciPrint [iteration]");
         }
+        int iteration = Integer.parseInt(args[0]);
         Fibonacci f = new Fibonacci();
-        System.out.println("| n |  f(n)  |");
-        System.out.println(f.next());
-        System.out.println(f.next());
-        System.out.println(f.next());
-        System.out.println(f.next());
-        System.out.println(f.next());
-        System.out.println(f.next());
-        System.out.println(f.next());
+        System.out.printf("| %2s %2s  %9s  %5s %n", "n", "|", "f(n)", "|");
+        System.out.println("+-----+-----------------+");
+        for (int i = 0; i <= iteration; i++) {
+          System.out.printf("| %2d %2s %15d %s %n", i, "|", f.next(), "|");
+        }
     }
 }
