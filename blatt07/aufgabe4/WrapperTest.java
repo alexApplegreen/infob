95baf9e6aff7c47a72e3ca78acac5f53cf9cfab3
lalilu1410 <lisa.strzelecki@web.de>
2018-06-12 20:24:21 +0200

    done

--- blatt07/aufgabe4/WrapperTest.java
+++ blatt07/aufgabe4/WrapperTest.java
@@ -1,35 +1,74 @@
 package aufgabe4;
 
-import java.io.*;
-
 public class WrapperTest {
     public static void main(String[] args) {
+        boolean passed = true;
         int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+
+        IntArrayWrapper wrapper1 = new IntArrayWrapper(array, "Test");
+
+        if (array.length != wrapper1.numberOfElements()) {
+            System.out.println("Instanciation did not work correctly");
+            passed = false;
+        }
+
+        if (wrapper1.getPos() != 0) {
+            System.out.println("getPos() does not work or constructor does not initialize pos correctly");
+            passed = false;
+        }
 
         for (int i = 0; i < array.length; i++) {
-            array[i] = i+1;
+            if (array[i] != wrapper1.getElement()) {
+                System.out.println("Elements are not correct");
+                passed = false;
+            }
+
+            if (wrapper1.getPos() != i) {
+                System.out.println("next() does not work");
+                passed = false;
+            }
+
+            wrapper1.next();
         }
 
-        IntArrayWrapper wrapper = new IntArrayWrapper(array, "Test");
-
-        System.out.println(wrapper.numberOfElements());
-
-        for (int i = 0; i < wrapper.numberOfElements(); i++) {
-
-            System.out.println(wrapper.getElement());
-            wrapper.next();
-
+        wrapper1.resetPos();
+        if (wrapper1.getPos() != 0) {
+            System.out.println("resetPos() does not work");
+            passed = false;
         }
 
-        wrapper.change(3, 5);
+        wrapper1.change(3, 5);
+        for (int i = 0; i < array.length; i++) {
+            if (i == 5) {
+                if (wrapper1.getElement() != 3) {
+                    System.out.println("change() does not work");
+                    passed = false;
+                }
+            }
+        }
 
-        wrapper.resetPos();
+        wrapper1.close();
+        try {
+            wrapper1.change(2, 4);
+            System.out.println("Closing file does not work");
+            passed = false;
+        } catch (RuntimeException e) {
+        }
 
-        for (int i = 0; i < wrapper.numberOfElements(); i++) {
+        IntArrayWrapper wrapper2 = new IntArrayWrapper("Test");
+        try {
+            wrapper2.change(2, 3);
+        } catch (RuntimeException e) {
+            System.out.println("Opening file from new IntArrayWrapper does not work");
+            passed = false;
+        }
 
-            System.out.println(wrapper.getElement());
-            wrapper.next();
 
+        if (passed) {
+            System.out.println("All tests passed");
         }
     }
 }
