801f036cc5fdb00b816dddae4dbbcae652630394
Alexander Tepe <alexander.tepe@hotmail.de>
2018-06-25 22:15:43 +0200

    has Bugs left, dunno.

--- Blatt09/aufgabe3/MultiSearch.java
+++ Blatt09/aufgabe3/MultiSearch.java
@@ -10,12 +10,21 @@ public class MultiSearch {
     private File f;
     private String regex;
 
+    /**
+     * @brief constructor
+     * @param recursive boolean walk subdirs or don't
+     * @param filepath String rootdir
+     * @param regex String pattern to search
+     */
     public MultiSearch(boolean recursive, String filepath, String regex) {
         this.recursive = recursive;
         this.f = new File(filepath);
         this.regex = regex;
     }
 
+    /**
+     * @brief perform parallel search in directory
+     */
     public void run() {
 
         FileSystem fs = new FileSystem(f);
@@ -23,13 +32,19 @@ public class MultiSearch {
         Queue<String> queue = new ConcurrentLinkedQueue<String>();
         ArrayList<File> list = new ArrayList<>();
 
+        // Use custom visitor to add every file in dir to arraylist
         MyFileVisitor v = new MyFileVisitor(list, recursive);
         fs.accept(v);
 
+        // new thread for every file in rootdir
         for (File file : list) {
 
             Thread t = new Thread(new Runnable() {
 
+                /**
+                 * @brief starts searches File line by line for given pattern and adds filename to queue if match is
+                 *        found
+                 */
                 @Override
                 public void run() {
                     try {
@@ -54,6 +69,7 @@ public class MultiSearch {
             t.start();
         }
 
+        // print every entry in queue
         for (String s : queue) {
             System.out.println(s);
         }
@@ -61,9 +77,36 @@ public class MultiSearch {
 
     public static void main(String[] args) {
 
-        // TODO parse args
+        boolean recursive = false;
+        String path = null;
+        String pattern = null;
 
-        MultiSearch ms = new MultiSearch(true, args[0], "");
+        if (args.length < 1) {
+            System.out.println("java MultiSearch [-r] {path} [-p pattern]");
+        }
+
+        int i = 0;
+        do {
+            if (args[i].equals("-r")) {
+                recursive = true;
+            }
+            if (args[i].equals("-p")) {
+                pattern = args[i+1];
+                i++;
+            } else {
+                path = args[i];
+            }
+            i++;
+        } while (i < args.length);
+
+        if (path == null) {
+            throw new RuntimeException("No File given!");
+        }
+        if (pattern == null) {
+            pattern = "";
+        }
+
+        MultiSearch ms = new MultiSearch(recursive, path, pattern);
         ms.run();
     }
 }
